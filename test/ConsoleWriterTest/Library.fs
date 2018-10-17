ac2923d0aaeb8139c5940ba2bd640784c03fc4fd
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-10-18 00:34:39 +1000

    add better tests for the console writer

--- test/ConsoleWriterTest/Library.fs
+++ test/ConsoleWriterTest/Library.fs
@@ -2,13 +2,24 @@ module Forest.Test.ConsoleWriter.Library
 
 open NUnit.Framework
 open FsUnit
+open System
+open System.IO
+
+let withConsole setup assertion =
+  use writer = new StringWriter()
+  Console.SetOut(writer);
+  setup()
+  assertion(writer.ToString())
 
 [<Test>]
-let ``Hope and pray``() =
-  let mutable thing = ""
-  let print msg = thing <- (sprintf msg)
-  let cw = (new Forest.ConsoleWriter.ConsoleWriter("nothing", false, print) :> Forest.ITarget)
-  thing |> should equal ""
-  cw.Write "LogName" Forest.Log "Test Log"
-  thing |> should equal "LogName"
-
+let ``The writer should be able to handle simple outputs``() =
+  let cw = (new Forest.ConsoleWriter.ConsoleWriter("nothing", false) :> Forest.ITarget)
+  withConsole(fun _ ->
+    cw.Write "LogName" Forest.Log "Test Log"
+  )(fun output -> output |> should equal "\u001b[0;34m[LogName] LOG: Test Log\u001b[0m\r\n")
+  withConsole(fun _ ->
+    cw.Write "LogName" Forest.Warn "Test Warn"
+  )(fun output -> output |> should equal "\u001b[0;33m[LogName] WARN: Test Warn\u001b[0m\r\n")
+  withConsole(fun _ ->
+    cw.Write "LogName" Forest.Error "Test Error"
+  )(fun output -> output |> should equal "\u001b[0;31m[LogName] ERROR: Test Error\u001b[0m\r\n")
